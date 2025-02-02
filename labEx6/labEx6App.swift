e9428ef9011da8e6790caf9b8b66149d41b2d17c
Gone-M <metcivan@gmail.com>
2025-02-02 16:11:22 -0500

    Initial Commit

---
+++ labEx6/labEx6App.swift
@@ -0,0 +1,17 @@
+//
+//  labEx6App.swift
+//  labEx6
+//
+//  Created by Civan Metin on 2025-02-02.
+//
+
+import SwiftUI
+
+@main
+struct labEx6App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ labEx6Tests/labEx6Tests.swift
@@ -0,0 +1,17 @@
+//
+//  labEx6Tests.swift
+//  labEx6Tests
+//
+//  Created by Civan Metin on 2025-02-02.
+//
+
+import Testing
+@testable import labEx6
+
+struct labEx6Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
